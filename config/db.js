44cb25108f4b85575c23f8f6c7c819a88fc5b2de
Shiko Matlala <shikomatlala@gmail.com>
2022-11-02 11:04:39 +0200

    Password Added

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize(
     'researcherdna_db_v2',
     'root',
-    '',
+    'Aj0y2RUKMG7x',
     {
         host: 'localhost',
         dialect: 'mysql'
